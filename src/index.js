b0c2c6f9a49a9c8a420d318cdcb2d12898dc0835
Qujuan Miller <qujuan.miller@snhu.edu>
2023-05-28 12:56:31 -0400

    Created redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "LOGGED_IN_USER":
+      return { ...state, ...action.payload };
+    case "LOGOUT":
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+//
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+
+//
+const store = createStore(rootReducer, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
